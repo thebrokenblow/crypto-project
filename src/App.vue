9be85e12cd550ea82d631610ec102676568b4ec4
Artem <artkrasov@gmail.com>
2023-07-17 01:01:51 +0300

    Eliminated code duplication

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div class="container">
+      <div class="w-full my-4"></div>
       <section>
         <div class="flex">
           <div class="max-w-xs">
@@ -10,6 +11,7 @@
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                 v-model="ticker"
+                @keydown.enter="add(this.ticker)"
                 @input="changeNameTicker"
                 type="text"
                 name="wallet"
@@ -18,28 +20,22 @@
                 placeholder="Например DOGE"
               />
             </div>
-            <div
-              v-if="similarNamesTikers.length"
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+            <span
+              v-for="(itemTiker, id) in similarNamesTikers"
+              v-bind:key="id"
+              @click="add(itemTiker.Symbol)"
+              class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
             >
-              <span
-                v-for="(item, id) in similarNamesTikers"
-                v-bind:key="id"
-                @click="addTicker(item.Symbol)"
-                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
-              >
-                {{ item.Symbol }}
-              </span>
-            </div>
-            <div v-if="flagContainsTickerInArraty" class="text-sm text-red-600">
+              {{ itemTiker.Symbol }}
+            </span>
+            <div v-if="isContainsTickerInArray" class="text-sm text-red-600">
               Такой тикер уже добавлен
             </div>
           </div>
         </div>
         <button
-          type="button"
-          v-on:keydown.enter="add"
           @click="add"
+          type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
           <!-- Heroicon name: solid/mail -->
@@ -58,30 +54,30 @@
           Добавить
         </button>
       </section>
-      <template v-if="tickers.length > 0">
+
+      <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="tickerItem in tickers"
-            v-bind:key="tickerItem.name"
-            @click="select(tickerItem)"
-            :class="{ 'border-4': state === tickerItem }"
+            v-for="ticker in tickers"
+            :key="ticker.name"
+            @click="select(ticker)"
+            :class="{
+              'border-4': sel === ticker,
+            }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ tickerItem.name }} - USD
+                {{ ticker.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ tickerItem.price }}
+                {{ ticker.price }}
               </dd>
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @:click.stop="
-                deleteItem(tickerItem);
-                state = null;
-              "
+              @click.stop="handleDelete(ticker)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -101,48 +97,47 @@
           </div>
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
-        <section v-if="state" class="relative">
-          <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-            {{ state.name }} - USD
-          </h3>
-          <div class="flex items-end border-gray-600 border-b border-l h-64">
-            <div
-              v-for="(bar, idx) in normalizeGraph()"
-              :key="idx"
-              :style="{ height: `${bar}%` }"
-              class="bg-purple-800 border w-10"
-            ></div>
-          </div>
-
-          <button
-            @click="state = null"
-            type="button"
-            class="absolute top-0 right-0"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              xmlns:svgjs="http://svgjs.com/svgjs"
-              version="1.1"
-              width="30"
-              height="30"
-              x="0"
-              y="0"
-              viewBox="0 0 511.76 511.76"
-              style="enable-background: new 0 0 512 512"
-              xml:space="preserve"
-            >
-              <g>
-                <path
-                  d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                  fill="#718096"
-                  data-original="#000000"
-                ></path>
-              </g>
-            </svg>
-          </button>
-        </section>
       </template>
+      <section v-if="sel" class="relative">
+        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+          {{ sel.name }} - USD
+        </h3>
+        <div class="flex items-end border-gray-600 border-b border-l h-64">
+          <div
+            v-for="(bar, idx) in normalizeGraph()"
+            :key="idx"
+            :style="{ height: `${bar}%` }"
+            class="bg-purple-800 border w-10"
+          ></div>
+        </div>
+        <button
+          @click="sel = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns:svgjs="http://svgjs.com/svgjs"
+            version="1.1"
+            width="30"
+            height="30"
+            x="0"
+            y="0"
+            viewBox="0 0 511.76 511.76"
+            style="enable-background: new 0 0 512 512"
+            xml:space="preserve"
+          >
+            <g>
+              <path
+                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                fill="#718096"
+                data-original="#000000"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      </section>
     </div>
   </div>
 </template>
@@ -150,15 +145,16 @@
 <script>
 export default {
   name: "App",
+
   data() {
     return {
       ticker: "",
       tickers: [],
-      state: null,
+      sel: null,
       graph: [],
       allTickerApp: [],
       similarNamesTikers: [],
-      flagContainsTickerInArraty: false,
+      isContainsTickerInArray: false,
     };
   },
 
@@ -173,17 +169,16 @@ export default {
   },
 
   methods: {
-    addTicker(nameTicker) {
+    add(tickerName) {
       const currentTicker = {
-        name: nameTicker.toLocaleUpperCase(),
+        name: tickerName.toLocaleUpperCase(),
         price: "-",
       };
       if (this.containsTickerInArray(currentTicker)) {
-        this.flagContainsTickerInArraty = true;
+        this.isContainsTickerInArray = true;
         return;
       } else {
         this.tickers.push(currentTicker);
-
         setInterval(async () => {
           const f = await fetch(
             `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=0afe0210a760fda9bd83b4cbfae971fe47715ca7b7218771af22be0f9465e2ac`
@@ -191,22 +186,14 @@ export default {
           const data = await f.json();
           this.tickers.find((t) => t.name === currentTicker.name).price =
             data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.state?.name === currentTicker.name) {
+
+          if (this.sel?.name === currentTicker.name) {
             this.graph.push(data.USD);
           }
         }, 5000);
-
         this.ticker = "";
         this.similarNamesTikers = [];
       }
-    },
-
-    containsTickerInArray(currentTicker) {
-      return this.tickers.some(
-        (ticker) =>
-          ticker.name.toLocaleUpperCase() ===
-          currentTicker.name.toLocaleUpperCase()
-      );
     },
 
     getSimilarNamesTikers(currentTicker) {
@@ -220,44 +207,30 @@ export default {
         .slice(0, 4);
     },
 
-    add() {
-      const currentTicker = {
-        name: this.ticker.toLocaleUpperCase(),
-        price: "-",
-      };
-      if (this.containsTickerInArray(currentTicker)) {
-        this.flagContainsTickerInArraty = true;
-        return;
-      } else {
-        this.tickers.push(currentTicker);
-
-        setInterval(async () => {
-          const f = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=0afe0210a760fda9bd83b4cbfae971fe47715ca7b7218771af22be0f9465e2ac`
-          );
-          const data = await f.json();
-          this.tickers.find((t) => t.name === currentTicker.name).price =
-            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.state?.name === currentTicker.name) {
-            this.graph.push(data.USD);
-          }
-        }, 5000);
-
-        this.ticker = "";
-      }
+    select(ticker) {
+      this.sel = ticker;
+      this.graph = [];
     },
+
     changeNameTicker() {
       if (this.ticker === "") {
         this.similarNamesTikers = [];
         return;
       }
       this.similarNamesTikers = this.getSimilarNamesTikers(this.ticker);
-      console.log(this.similarNamesTikers);
-      this.flagContainsTickerInArraty = false;
+      this.isContainsTickerInArray = false;
     },
-    select(ticker) {
-      this.state = ticker;
-      this.graph = [];
+
+    handleDelete(tickerToRemove) {
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+    },
+
+    containsTickerInArray(currentTicker) {
+      return this.tickers.some(
+        (ticker) =>
+          ticker.name.toLocaleUpperCase() ===
+          currentTicker.name.toLocaleUpperCase()
+      );
     },
 
     normalizeGraph() {
@@ -266,10 +239,6 @@ export default {
       return this.graph.map(
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
-    },
-
-    deleteItem(tickerItem) {
-      this.tickers = this.tickers.filter((t) => t != tickerItem);
     },
   },
 };
